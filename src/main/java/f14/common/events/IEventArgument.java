50bc2eae155cc8b7a4d880fc103fc5a0701b169b
f14shm4n <uraborg@gmail.com>
2017-11-26 15:50:47 +0300

    [deleted] EventListenerPool classes. [moved] CollectionUtil -> root level of package.

--- src/main/java/f14/common/events/IEventArgument.java
+++
@@ -1,8 +0,0 @@
-package f14.common.events;
-
-/**
- * Created by f14shm4n on 15.11.2017.
- */
-
-public interface IEventArgument {
-}
--- src/main/java/f14/common/events/IEventListener.java
+++
@@ -1,9 +0,0 @@
-package f14.common.events;
-
-/**
- * Created by f14shm4n on 15.11.2017.
- */
-
-public interface IEventListener<E extends IEventArgument> {
-    void handle(E arg) throws Throwable;
-}
