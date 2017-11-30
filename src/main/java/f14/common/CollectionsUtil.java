a44c4582f28353518979f9787a98f47c47f7ab93
f14shm4n <uraborg@gmail.com>
2017-11-30 22:09:31 +0300

    [added] Collection max, min methods.

--- src/main/java/f14/common/CollectionsUtil.java
+++ src/main/java/f14/common/CollectionsUtil.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import f14.common.functions.Action1;
-import f14.common.functions.Predicate;
+import f14.common.functions.*;
 
 /**
  * Created by f14shm4n on 15.11.2017.
@@ -55,5 +54,113 @@ public final class CollectionsUtil {
             }
         }
         return false;
+    }
+
+    public static <T> byte maxByte(Collection<T> collection, Function1<T, Byte> selector) {
+        byte value = Byte.MIN_VALUE;
+        for (T o : collection) {
+            byte curr = selector.run(o);
+            value = (byte) Math.max(curr, value);
+        }
+        return value;
+    }
+
+    public static <T> short maxShort(Collection<T> collection, Function1<T, Short> selector) {
+        short value = Short.MIN_VALUE;
+        for (T o : collection) {
+            short curr = selector.run(o);
+            value = (short) Math.max(curr, value);
+        }
+        return value;
+    }
+
+    public static <T> int maxInt(Collection<T> collection, Function1<T, Integer> selector) {
+        int value = Integer.MIN_VALUE;
+        for (T o : collection) {
+            int curr = selector.run(o);
+            value = Math.max(curr, value);
+        }
+        return value;
+    }
+
+    public static <T> long maxLong(Collection<T> collection, Function1<T, Long> selector) {
+        long value = Long.MIN_VALUE;
+        for (T o : collection) {
+            long curr = selector.run(o);
+            value = Math.max(curr, value);
+        }
+        return value;
+    }
+
+    public static <T> float maxFloat(Collection<T> collection, Function1<T, Float> selector) {
+        float value = Float.MIN_VALUE;
+        for (T o : collection) {
+            float curr = selector.run(o);
+            value = Math.max(curr, value);
+        }
+        return value;
+    }
+
+    public static <T> double maxDouble(Collection<T> collection, Function1<T, Double> selector) {
+        double value = Double.MIN_VALUE;
+        for (T o : collection) {
+            double curr = selector.run(o);
+            value = Math.max(curr, value);
+        }
+        return value;
+    }
+
+    public static <T> byte minByte(Collection<T> collection, Function1<T, Byte> selector) {
+        byte value = Byte.MAX_VALUE;
+        for (T o : collection) {
+            byte curr = selector.run(o);
+            value = (byte) Math.min(curr, value);
+        }
+        return value;
+    }
+
+    public static <T> short minShort(Collection<T> collection, Function1<T, Short> selector) {
+        short value = Short.MAX_VALUE;
+        for (T o : collection) {
+            short curr = selector.run(o);
+            value = (short) Math.min(curr, value);
+        }
+        return value;
+    }
+
+    public static <T> int minInt(Collection<T> collection, Function1<T, Integer> selector) {
+        int value = Integer.MAX_VALUE;
+        for (T o : collection) {
+            int curr = selector.run(o);
+            value = Math.min(curr, value);
+        }
+        return value;
+    }
+
+    public static <T> long minLong(Collection<T> collection, Function1<T, Long> selector) {
+        long value = Long.MAX_VALUE;
+        for (T o : collection) {
+            long curr = selector.run(o);
+            value = Math.min(curr, value);
+        }
+        return value;
+    }
+
+    public static <T> float minFloat(Collection<T> collection, Function1<T, Float> selector) {
+        float value = Float.MAX_VALUE;
+        for (T o : collection) {
+            float curr = selector.run(o);
+            value = Math.min(curr, value);
+        }
+        return value;
+    }
+
+    public static <T> double minDouble(Collection<T> collection, Function1<T, Double> selector) {
+        double value = Double.MAX_VALUE;
+        for (T o : collection) {
+            double curr = selector.run(o);
+            value = Math.min(curr, value);
+        }
+        return value;
     }
 }
--- src/test/java/CollectionUtilTest.java
+++ src/test/java/CollectionUtilTest.java
@@ -5,8 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
-import f14.common.functions.Action;
+import java.util.Random;
 
 /**
  * Created by f14shm4n on 15.11.2017.
@@ -26,4 +25,56 @@ public class CollectionUtilTest {
 
         Assert.assertEquals(2, listTypeOf.size());
     }
+
+    @Test
+    public void maxInt() {
+        List<Sample> list = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            list.add(Sample.create(i));
+        }
+
+        int max = CollectionsUtil.maxInt(list, o -> o.IntVal);
+        Assert.assertEquals(10, max);
+    }
+
+    @Test
+    public void minInt() {
+        List<Sample> list = new ArrayList<>();
+        list.add(Sample.create(0));
+        list.add(Sample.create(1));
+        list.add(Sample.create(3));
+        list.add(Sample.create(5));
+
+        int max = CollectionsUtil.minInt(list, o -> o.IntVal);
+        Assert.assertEquals(0, max);
+    }
+
+    @Test
+    public void minDouble() {
+        List<Sample> list = new ArrayList<>();
+        list.add(Sample.create(0.13));
+        list.add(Sample.create(0.11));
+        list.add(Sample.create(6));
+        list.add(Sample.create(1.12));
+
+        double max = CollectionsUtil.minDouble(list, o -> o.DoubleVal);
+        Assert.assertEquals(0.11, max, 0);
+    }
+
+    private static class Sample {
+        public int IntVal;
+        public long LongVal;
+        public float FloatVal;
+        public double DoubleVal;
+
+        public static <T extends Number> Sample create(T val) {
+            Sample t = new Sample();
+            t.IntVal = val.intValue();
+            t.LongVal = val.longValue();
+            t.FloatVal = val.floatValue();
+            t.DoubleVal = val.doubleValue();
+            return t;
+        }
+    }
+
 }
